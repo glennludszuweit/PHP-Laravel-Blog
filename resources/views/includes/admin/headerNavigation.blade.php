39881b5358f9007f0a1f873751be620c94c9d27d
glenn.ludszuweit <glenn.ludszuweit@outlook.com>
2020-01-03 02:11:46 +0100

    updated login and register pages using modal

--- resources/views/includes/admin/headerNavigation.blade.php
+++ resources/views/includes/admin/headerNavigation.blade.php
@@ -3,7 +3,7 @@
         <i class="fa fa-bars"></i>
     </a>
 
-    <a class="navbar-brand" href="#">
+    <a class="navbar-brand pl-5" href="#">
         <img src="{{ asset('admin/assets/imgs/logo.svg') }}" alt="logo">
     </a>
 
@@ -12,24 +12,10 @@
     </a>
 
     <ul class="navbar-nav ml-auto">
-        <li class="nav-item d-md-down-none">
-            <a href="#">
-                <i class="fa fa-bell"></i>
-                <span class="badge badge-pill badge-danger">5</span>
-            </a>
-        </li>
-
-        <li class="nav-item d-md-down-none">
-            <a href="#">
-                <i class="fa fa-envelope-open"></i>
-                <span class="badge badge-pill badge-danger">5</span>
-            </a>
-        </li>
-
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <img src="{{ asset('admin/assets/imgs/avatar-1.png') }}" class="avatar avatar-sm" alt="logo">
-                <span class="small ml-1 d-md-down-none">John Smith</span>
+                <span class="small ml-1 d-md-down-none">{{ Auth::user()->name }}</span>
             </a>
 
             <div class="dropdown-menu dropdown-menu-right">
@@ -38,22 +24,8 @@
                 <a href="#" class="dropdown-item">
                     <i class="fa fa-user"></i> Profile
                 </a>
-
-                <a href="#" class="dropdown-item">
-                    <i class="fa fa-envelope"></i> Messages
-                </a>
-
-                <div class="dropdown-header">Settings</div>
-
-                <a href="#" class="dropdown-item">
-                    <i class="fa fa-bell"></i> Notifications
-                </a>
-
-                <a href="#" class="dropdown-item">
-                    <i class="fa fa-wrench"></i> Settings
-                </a>
-
-                <a href="#" class="dropdown-item">
+                <form method="post" id="logout-form" action="{{ route('logout') }}">@csrf</form>
+                <a href="#" class="dropdown-item" onclick="document.getElementById('logout-form').submit();">
                     <i class="fa fa-lock"></i> Logout
                 </a>
             </div>
