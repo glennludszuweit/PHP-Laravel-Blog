333874b16917bfa7a663dfb203fdf2ac44eb814a
glenn.ludszuweit <glenn.ludszuweit@outlook.com>
2020-01-06 18:31:17 +0100

    updated user dashboard pages

--- resources/views/user/comments.blade.php
+++ resources/views/user/comments.blade.php
@@ -1,2 +1,47 @@
 @extends('layouts.admin')
-User Comments
+
+@section('title')
+    User Comments
+@endsection
+
+@section('content')
+    <div class="content">
+        <div class="card">
+            <div class="card-header bg-light">
+                Striped Rows
+            </div>
+
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-striped">
+                        <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Post</th>
+                            <th>Content</th>
+                            <th>Created at</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach(Auth::user()->comments as $comment)
+                            <tr>
+                                <td>{{ $comment->id }}</td>
+                                <td><a href="{{ route('singlePost', $comment->id) }}">{{ $comment->post->title }}</a></td>
+                                <td><a href="{{ route('singlePost', $comment->id) }}">{{ str_limit($comment->content, 20) }}</a></td>
+                                <td>{{ \Carbon\Carbon::parse($comment->created_at)->diffForHumans() }}</td>
+                                <td>
+                                    <form id="deleteComment-{{ $comment->id }}" action="{{ route('deleteComment', $comment->id) }}" method="post">
+                                        @csrf
+                                        <a  href="#" style="color: red;" onclick="document.getElementById('deleteComment-{{ $comment->id }}').submit()">X</a>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
